66c90379398c7a40b54aebcfa4c818e8cffe6704
Markus Liebscher <markusliebscher2@gmail.com>
2020-04-15 17:44:43 +0200

    rotation 2 Z

--- Tetris/Pieces.cs
+++ Tetris/Pieces.cs
@@ -115,21 +115,21 @@ namespace Tetris
             }
             if (rotation == 1)
             {
-                output[1] = Tuple.Create(position.Item1 + 1, position.Item2 + 1);
-                output[2] = Tuple.Create(position.Item1, position.Item2 - 1);
-                output[3] = Tuple.Create(position.Item1 + 1, position.Item2);
+                output[1] = Tuple.Create(position.Item1, position.Item2 + 1);
+                output[2] = Tuple.Create(position.Item1 + 1, position.Item2);
+                output[3] = Tuple.Create(position.Item1 + 1, position.Item2 - 1);
             }
             if (rotation == 2)
             {
-                output[1] = Tuple.Create(position.Item1 - 1, position.Item2 );
-                output[2] = Tuple.Create(position.Item1, position.Item2 - 1);
-                output[3] = Tuple.Create(position.Item1 + 1, position.Item2 - 1);
+                output[1] = Tuple.Create(position.Item1 + 1, position.Item2);
+                output[2] = Tuple.Create(position.Item1 - 1, position.Item2);
+                output[3] = Tuple.Create(position.Item1 - 1, position.Item2 - 1);
             }
             if (rotation == 3)
             {
-                output[1] = Tuple.Create(position.Item1, position.Item2 + 1);
+                output[1] = Tuple.Create(position.Item1 - 1, position.Item2 + 1);
                 output[2] = Tuple.Create(position.Item1 - 1, position.Item2);
-                output[3] = Tuple.Create(position.Item1 - 1, position.Item2 - 1);
+                output[3] = Tuple.Create(position.Item1, position.Item2 - 1);
             }
 
 
@@ -149,9 +149,9 @@ namespace Tetris
             }
             if (rotation == 1)
             {
-                output[1] = Tuple.Create(position.Item1, position.Item2 + 1);
-                output[2] = Tuple.Create(position.Item1 + 1, position.Item2);
-                output[3] = Tuple.Create(position.Item1 + 1, position.Item2 - 1);
+                output[1] = Tuple.Create(position.Item1 + 1, position.Item2);
+                output[2] = Tuple.Create(position.Item1 + 1, position.Item2 + 1);
+                output[3] = Tuple.Create(position.Item1, position.Item2 - 1);
             }
             if (rotation == 2)
             {
